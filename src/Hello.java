b878f52b9616a450f3da62667c3015b63232c432
Milici-Ilie <milicicristyan@yahoo.com>
2024-03-05 00:56:46 +0200

    && operator

--- src/Hello.java
+++ src/Hello.java
@@ -7,9 +7,18 @@ public class Hello {
         if (isAlien == false) {
             System.out.println("It is not an alien!");
         } else {
-            System.out.println("It is an alien predator!!!");
+            System.out.println("It is an alien " + "predator!!!");
         }
 
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greate than second top score and less than 100 ");
+        }
 
     }
 }
