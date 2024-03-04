683bd0aabb5f67af9c2f45e2e8d425fe4e542a01
Milici-Ilie <milicicristyan@yahoo.com>
2024-03-04 23:54:50 +0200

    creating a boolean

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Cristian");
+
+        boolean isAlien = true;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+        } else {
+            System.out.println("It is an alien predator!!!");
+        }
+
+
+    }
+}
+
+
